bfdf477a22a6d934b44223d9aaf8ef23c8044ff5
Gabriel Morais <77515545+G-Morais@users.noreply.github.com>
2022-05-19 10:23:52 -0300

    concluido

--- app.js
+++ app.js
@@ -1,31 +1,29 @@
+const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
+
 const fetchPokemon = () =>{
-    const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
+    const generatePokemonPromises = () => Array(150).fill().map((_, index) =>
+    fetch(getPokemonUrl(index + 1)).then(response => response.json()))
 
-    const pokemonPromises = []
-
-    for(let i = 1; i <= 150; i++){
-        pokemonPromises.push(fetch(getPokemonUrl(i)).then(response => response.json()))
-    }
+    const pokemonPromises = generatePokemonPromises()
 
     Promise.all(pokemonPromises)
     .then(pokemons => {
-        //console.log(pokemons)
-
-        const listPokemons = pokemons.reduce((accumultor, pokemon) => {
+        const lisPokemons = pokemons.reduce((accumulator, pokemon) => {
             const types = pokemon.types.map(typeInfo => typeInfo.type.name)
-            accumultor += `
 
-        <li class="card">
-        <img class="card-image ${types[0]}" alt="${pokemon.name}" src ="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}" />
+            accumulator += `
+        <li class="card" ${types[0]}>
+        <img class="card-image" alt="${pokemon.name}" src ="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"/>
             <h2 class ="card-title">${pokemon.id}. ${pokemon.name}</h2>
-            <p class ="card-subtitle">${ypes.join(' | ')}</p>
-        </li>`
-            return accumultor
+            <p class ="card-subtitle">${types.join(' | ')}</p>
+        </li>
+        `
+            return accumulator
         }, '')
 
-        const ul = document.querySelector('[data-js="pokedex"]')
+        const ul = document.querySelector('[data-js="pokedex"]');
 
-        console.log(ul.innerHTML)
+        ul.innerHTML = lisPokemons
     })
 }
 
